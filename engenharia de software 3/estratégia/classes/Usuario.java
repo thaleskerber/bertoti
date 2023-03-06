ba1c56be56333dd7c9973887a665decb61d28004
Thales Kerber <thaleskerber@gmail.com>
2023-03-06 09:41:35 -0300

    Add files via upload

--- engenharia de software 3/estratégia/classes/Usuario.java
+++ engenharia de software 3/estratégia/classes/Usuario.java
@@ -1,13 +1,21 @@
 package padraoestrategia;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Usuario {
    
     private String nome;
     private TipoUsuario tipoUsuario;
+    private List<String> notificacoes;
     
     public Usuario(String nome,TipoUsuario tipoUsuario){
         this.nome = nome;
         this.tipoUsuario = tipoUsuario;
+        this.notificacoes = new ArrayList<String>();
+        List<Usuario> usuarios = PadraoEstrategia.getControleAcesso().getUsuarios();
+        usuarios.add(this);
+        PadraoEstrategia.getControleAcesso().setUsuarios(usuarios);
     }
 
     public String getNome() {
@@ -20,5 +28,17 @@ public class Usuario {
     
     public void acessar(){
         tipoUsuario.acessar();
+    }
+    
+    public List<String> getNotificacoes(){
+        return notificacoes;
+    }
+    
+    public void adicionarNotificacao(String message){
+        notificacoes.add(message);
+    }
+    
+    public void setTipoUsuario(TipoUsuario tipoUsuario){
+        this.tipoUsuario = tipoUsuario;
     }
 }
